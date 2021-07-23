03f9de7d12e6314dd2c81c021973af64f4cf63ff
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-07-23 18:22:37 +0300

    Initial commit

---
+++ app/Http/Controllers/MasterController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Master;
+use Illuminate\Http\Request;
+
+class MasterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Master  $master
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Master $master)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Master  $master
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Master $master)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Master  $master
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Master $master)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Master  $master
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Master $master)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/OutfitController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Outfit;
+use Illuminate\Http\Request;
+
+class OutfitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Outfit  $outfit
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Outfit $outfit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Outfit  $outfit
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Outfit $outfit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Outfit  $outfit
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Outfit $outfit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Outfit  $outfit
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Outfit $outfit)
+    {
+        //
+    }
+}
