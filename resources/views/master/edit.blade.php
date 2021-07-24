21db29654254658e806d3ba0b6b531dedb3e36c9
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-07-24 15:17:01 +0300

    outfits masters

--- resources/views/master/edit.blade.php
+++ resources/views/master/edit.blade.php
@@ -1,7 +1,23 @@
-<form method="POST" action="{{route('master.update',$master)}}">
-    Name: <input type="text" name="master_name" value="{{$master->name}}">
-    Surname: <input type="text" name="master_surname" value="{{$master->surname}}">
-    @csrf
-    <button type="submit">EDIT</button>
- </form>
- 
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+   <div class="row justify-content-center">
+       <div class="col-md-8">
+           <div class="card">
+               <div class="card-header">Edit new master</div>
+
+               <div class="card-body">
+                <form method="POST" action="{{route('master.update',$master)}}">
+                    Name: <input type="text" name="master_name" value="{{$master->name}}">
+                    Surname: <input type="text" name="master_surname" value="{{$master->surname}}">
+                    @csrf
+                    <button type="submit">EDIT</button>
+                 </form>
+                 
+               </div>
+           </div>
+       </div>
+   </div>
+</div>
+@endsection
--- resources/views/master/index.blade.php
+++ resources/views/master/index.blade.php
@@ -1,10 +1,27 @@
-@foreach ($masters as $master)
-  <a href="{{route('master.edit',[$master])}}">{{$master->name}} {{$master->surname}}</a>
-  <form method="POST" action="{{route('master.destroy', $master)}}">
-   @csrf
-   <button type="submit">DELETE</button>
-  </form>
-  <br>
-@endforeach
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+   <div class="row justify-content-center">
+       <div class="col-md-8">
+           <div class="card">
+               <div class="card-header">Masters</div>
+
+               <div class="card-body">
+                @foreach ($masters as $master)
+                <a href="{{route('master.edit',[$master])}}">{{$master->name}} {{$master->surname}}</a>
+                <form method="POST" action="{{route('master.destroy', $master)}}">
+                 @csrf
+                 <button type="submit">DELETE</button>
+                </form>
+                <br>
+              @endforeach
+              
+               </div>
+           </div>
+       </div>
+   </div>
+</div>
+@endsection
 
 
