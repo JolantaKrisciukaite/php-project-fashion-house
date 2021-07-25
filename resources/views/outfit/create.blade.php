c0834691f124d175c1ea2c93c9cfc704b652a29a
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-07-25 13:38:17 +0300

    span css

--- resources/views/outfit/create.blade.php
+++ resources/views/outfit/create.blade.php
@@ -13,27 +13,33 @@
                     <div class="form-group">
                         <label>Type:</label>
                         <input type="text" name="outfit_type" class="form-control" value="{{old('outfit_type')}}">
+                        <span style="createSpan">Enter outfit type</span>
                     </div>
 
                     <div class="form-group">
                         <label>Color:</label>
                         <input type="text" name="outfit_color" class="form-control" value="{{old('outfit_color')}}">
+                        <span style="createSpan">Enter outfit color</span>
                     </div>
 
                     <div class="form-group">
                         <label>Size:</label>
                         <input type="text" name="outfit_size" class="form-control" value="{{old('outfit_size')}}">
+                        <span style="createSpan">Enter outfit size</span>
                     </div>
                     
                     <span class="about">About: </span>
                     <textarea name="outfit_about" id="summernote" value="{{old('outfit_about')}}"></textarea>
                     <select class="index" name="master_id"><br>
+                        
                         @foreach ($masters as $master)
                             <option value="{{$master->id}}">{{$master->name}} {{$master->surname}}</option>
                         @endforeach
-                 </select>
+                        <span style="createSpan">About outfit</span>
+                    </select>
                     @csrf
-                    <button class="addButtonCreate" type="submit">Add</button>
+                    <button class="addButtonCreate" type="submit">Add</button><br>
+                    <span style="createSpan">Select Master from the list</span>
                  </form>
 
                </div>
