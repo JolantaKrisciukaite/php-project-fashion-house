b0f6d2177101d41590f86e50ad77a75c7afc128d
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-07-25 18:10:34 +0300

    sorting css

--- resources/views/outfit/index.blade.php
+++ resources/views/outfit/index.blade.php
@@ -1,53 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-   <div class="row justify-content-center">
-       <div class="col-md-8">
-           <div class="card">
-               <div class="card-header">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
 
-                   <h3 class="title">Outfits</h3>
-                   <form action="{{route('outfit.index')}}" method="get" class="sort-form">
-                    <fieldset>
-                        <legend>Sort by</legend>
-                        <div>
-                        <label>Type</label><input type="radio" name="sort_by" value="type" checked>
-                        <label>Size</label><input type="radio" name="sort_by" value="size">
-                        </div>
-                    </fieldset>
+                        <h3 class="title">Outfits</h3>
+                        <form action="{{ route('outfit.index') }}" method="get" class="sort-form">
+                            <fieldset>
+                                <legend>Sort by</legend>
+                                <div>
+                                    <label>Type</label><input type="radio" name="sort_by" value="type" checked>
+                                    <label>Size</label><input type="radio" name="sort_by" value="size">
+                                </div>
+                            </fieldset>
 
-                    <fieldset>
-                        <legend>Direction</legend>
-                        <div>
-                            <label>Asc</label><input type="radio" name="dir" value="asc" checked>
-                            <label>Dsc</label><input type="radio" name="dir" value="desc">
-                        </div>
-                    </fieldset>
-                    <button class="addButtonCreate" type="submit">Sort</button>
-                   </form>
-                
+                            <fieldset>
+                                <legend>Direction</legend>
+                                <div>
+                                    <label>Asc</label><input type="radio" name="dir" value="asc" checked>
+                                    <label>Dsc</label><input type="radio" name="dir" value="desc">
+                                </div>
+                            </fieldset>
+                            <button class="addButtonCreate" type="submit">Sort</button>
+                            <a href="{{ route('outfit.index') }}" class="aButton">Clear</button></a>
+                        </form>
+
+                    </div>
+
+                    <div class="card-body">
+                        @foreach ($outfits as $outfit)
+                            <div class="index">Outfit: {{ $outfit->type }}</div>
+                            <div class="index">Outfit size: {{ $outfit->size }}</div>
+                            <div class="index">Master: {{ $outfit->outfitMaster->name }}
+                                {{ $outfit->outfitMaster->surname }}</div>
+                            <form method="POST" action="{{ route('outfit.destroy', [$outfit]) }}">
+                                <a href="{{ route('outfit.edit', [$outfit]) }}" class="editButton">Edit</a>
+                                @csrf
+                                <button class="deleteButtonOutfit" type="submit">Delete</button>
+                            </form><br>
+                        @endforeach
+
+                    </div>
                 </div>
-
-               <div class="card-body">
-                @foreach ($outfits as $outfit)
-                <div class="index">Outfit: {{$outfit->type}}</div>
-                <div class="index">Outfit size: {{$outfit->size}}</div>
-                <div class="index">Master: {{$outfit->outfitMaster->name}} {{$outfit->outfitMaster->surname}}</div>
-                <form method="POST" action="{{route('outfit.destroy', [$outfit])}}">
-                    <a href="{{route('outfit.edit',[$outfit])}}" class="editButton">Edit</a>
-                 @csrf
-                 <button class="deleteButtonOutfit" type="submit">Delete</button>
-                </form><br>
-              @endforeach
-              
-               </div>
-           </div>
-       </div>
-   </div>
-</div>
+            </div>
+        </div>
+    </div>
 @endsection
-
-
-
-
